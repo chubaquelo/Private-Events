201949ecdd5f4be0a497721ba002be2780c21cbf
Sergio <sergioobolevich@gmail.com>
2020-11-18 19:41:17 -0300

    Add past and future events display on Event Index View.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,13 @@ class Event < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :attendees
   has_many :users, through: :attendees
+
+  def self.past
+    Event.where("datetime <= Time.zone.now", {timenow: Time.zone.now})
+  end
+
+  def self.future
+    Event.where("datetime >= :timenow", {timenow: Time.zone.now})
+  end
+  
 end
