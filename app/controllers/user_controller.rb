850b2c50248975305c32fed072c2bdb3fe9035ad
Sergio <sergioobolevich@gmail.com>
2020-11-18 18:53:32 -0300

    Add Attended events in User show view.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,8 +5,6 @@ class UserController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @events = []
-    # @events = @user.events.all
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :attendees
-  has_many :attended_events, through: :attendees, foreign_key: 'event_id', class_name: 'Attendee'
+  has_many :attended_events, through: :attendees, foreign_key: 'event_id', :source => :event
+
   has_many :authored_events, foreign_key: 'author_id', class_name: 'Event'
 end
