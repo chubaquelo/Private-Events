fca66d6b6e2179916f792773acf3a450ec4e9526
ricardo <ramn21@gmail.com>
2020-11-23 21:32:01 +0100

    add test user model

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Create an Event' do
+
+    subject { Event.create( location: 'los angeles', datetime: '2020-11-23', author_id: 3)}
+
+    it 'change the count of events by one' do
+
+      expect{ subject }.to change{ Event.count }.by(1)
+    end
+
+    it 'return the location los angeles' do
+      expect(subject.location).to eq('los angeles')
+    end
+
+    it 'have many users' do
+      e = Event.reflect_on_association(:users)
+      expect(e.macro).to eq(:has_many)
+    end
+
+    it 'belong to author' do
+      e = Event.reflect_on_association(:author)
+      expect(e.macro).to eq(:belongs_to)
+    end
+
+    it 'have many attendees' do
+      e = Event.reflect_on_association(:attendees)
+      expect(e.macro).to eq(:has_many)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Create a user' do
+    subject {User.create(name: 'carlos')}
+
+    it 'change the count of users by one' do
+      expect{ subject }.to change{ User.count }.by(1)
+    end
+
+    it 'return carlos' do
+      expect(subject.name).to eq('carlos')
+    end
+
+    it 'have many attended_events' do
+      u = User.reflect_on_association(:attended_events)
+      expect(u.macro).to eq(:has_many)
+    end
+
+    it 'have many authored_events' do
+      u = User.reflect_on_association(:authored_events)
+      expect(u.macro).to eq(:has_many)
+    end
+
+    it 'have many authored_events' do
+      u = User.reflect_on_association(:authored_events)
+      expect(u.macro).to eq(:has_many)
+    end
+
+    it 'have many attendees' do
+      u = User.reflect_on_association(:attendees)
+      expect(u.macro).to eq(:has_many)
+    end
+  end
 end
