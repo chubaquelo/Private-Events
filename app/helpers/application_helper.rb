780a002d50a513737552782308b05ad545c37bac
ricardo <ramn21@gmail.com>
2020-11-24 20:51:01 +0100

    add helper for user view show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  
+  def something(events)
+    events.each do |event|
+      concat content_tag(:div, content_tag(:div,  content_tag(:div, content_tag(:h4, event.location, class: "card-title") + content_tag(:p, event.datetime, class: "card-title") + (link_to 'View event', event_path(event.id)), class: 'card-body'), class: 'card w-100 mb-3'), class: 'col-sm-3 d-flex align-items-stretch')
+    end
+  end
+
+  def next_something(event)
+    event.each do |event|
+      if event.datetime > Time.zone.now
+        concat content_tag(:div, content_tag(:div,  content_tag(:div, content_tag(:h4, event.location, class: "card-title") + content_tag(:p, event.datetime, class: "card-title") + (link_to 'View event', event_path(event.id)), class: 'card-body'), class: 'card w-100 mb-3'), class: 'col-sm-3 d-flex align-items-stretch')
+      end
+    end
+  end
+
+
+  def nex2(event)
+    event.each do |event|
+      if event.datetime < Time.zone.now
+        concat content_tag(:div, content_tag(:div,  content_tag(:div, content_tag(:h4, event.location, class: "card-title") + content_tag(:p, event.datetime, class: "card-title") + (link_to 'View event', event_path(event.id)), class: 'card-body'), class: 'card w-100 mb-3'), class: 'col-sm-3 d-flex align-items-stretch')
+      end
+    end  
+  end
+
 end
