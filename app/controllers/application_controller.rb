cfcfcbaab0a4e4d8ccd86c7fd6a34b6547a37f09
ricardo <ramn21@gmail.com>
2020-11-25 00:24:37 +0100

    fix Layout/EmptyLineBetweenDefs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= session[:user_id] &&
                       User.find_by(id: session[:user_id])
   end
+
   # rubocop:disable Style/DoubleNegation
   def signed_in?
     !!current_user
