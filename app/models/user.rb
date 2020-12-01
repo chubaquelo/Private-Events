a896575a73185770aaebd160873656f0cb25f61a
Sergio <sergioobolevich@gmail.com>
2020-12-01 00:58:13 -0300

    Add validation on user creation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :attended_events, through: :attendees, foreign_key: 'event_id', source: :event
 
   has_many :authored_events, foreign_key: 'author_id', class_name: 'Event'
+
+  validates :name, :presence => true, uniqueness: true, :length => {:minimum => 4, :message => 'Name is too short...'}
 end
